829a07180bf6eb58cf47093aa7aea880f684d969
Vinit-Nagar <vinitnagar21@gmail.com>
2024-06-12 14:15:28 +0530

    files setup and socket io realtime connection setup

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const socket = require("socket.io");
+const { Chess } = require("chess.js");
+const http = require("http");
+const path = require("path");
+const { title } = require("process");
+
+const app = express();
+const server = http.createServer(app);
+const io = socket(server);
+
+const chess = new Chess();
+let players = {};
+let currentPlayer = "W";
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.render("index", { title: "Chess Game" });
+});
+
+io.on("connection", function (uniquesoket) {
+  console.log("connected");
+});
+
+server.listen(3000, function () {
+  console.log("listening in port 3000");
+});
