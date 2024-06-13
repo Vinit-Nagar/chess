da784c95479b5be594427194b33af89c9b9b3a29
Vinit-Nagar <vinitnagar21@gmail.com>
2024-06-13 19:02:06 +0530

    chess board render

--- public/js/chessgame.js
+++ public/js/chessgame.js
@@ -5,3 +5,81 @@ const boardElement = document.querySelector(".chessboard");
 let draggedPiece = null;
 let sourceSquare = null;
 let playerRole = null;
+
+const renderBoard = () => {
+  const board = chess.board();
+  boardElement.innerHTML = "";
+  board.forEach((row, rowindex) => {
+    row.forEach((square, squareindex) => {
+      const squareElement = document.createElement("div");
+      squareElement.classList.add(
+        "square",
+        (rowindex + squareindex) % 2 == 0 ? "light" : "dark"
+      );
+      squareElement.dataset.row = rowindex;
+      squareElement.dataset.col = squareindex;
+
+      if (square) {
+        const pieceElement = document.createElement("div");
+
+        pieceElement.classList.add(
+          "piece",
+          square.color === "w" ? "white" : "black"
+        );
+        pieceElement.innerText = getPieceUnicode(square);
+        pieceElement.draggable = playerRole === square.color;
+
+        pieceElement.addEventListener("dragstart", (e) => {
+          if (pieceElement.draggable) {
+            draggedPiece = pieceElement;
+            sourceSquare = { row: rowindex, col: squareindex };
+            e.dataTransfer.setData("text/plain", "");
+          }
+        });
+
+        pieceElement.addEventListener("dragend", (e) => {
+          draggedPiece = null;
+          sourceSquare = null;
+        });
+        squareElement.appendChild(pieceElement);
+      }
+
+      squareElement.addEventListener("dragover", function (e) {
+        e.preventDefault();
+      });
+      squareElement.addEventListener("drop", function (e) {
+        e.preventDefault();
+        if (draggedPiece) {
+          const targetSource = {
+            row: parseInt(squareElement.dataset.row),
+            col: parseInt(squareElement.dataset.col),
+          };
+          handleMove(sourceSquare, targetSource);
+        }
+      });
+      boardElement.appendChild(squareElement);
+    });
+  });
+};
+
+const handleMove = () => {};
+
+const getPieceUnicode = (piece) => {
+  const unicodePieces = {
+    p: "♟︎",
+    r: "♜",
+    n: "♞",
+    b: "♝",
+    q: "♛",
+    k: "♚",
+    P: "♙",
+    R: "♖",
+    N: "♘",
+    B: "♗",
+    Q: "♕",
+    K: "♔",
+  };
+  return unicodePieces[piece.type] || "";
+};
+
+renderBoard();
