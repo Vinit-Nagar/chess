6a327782ce8be6cea4108b0eae31d704a7cf0ad3
Vinit-Nagar <vinitnagar21@gmail.com>
2024-06-13 16:33:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const io = socket(server);
 
 const chess = new Chess();
 let players = {};
-let currentPlayer = "W";
+let currentPlayer = "w";
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
@@ -22,6 +22,24 @@ app.get("/", (req, res) => {
 
 io.on("connection", function (uniquesoket) {
   console.log("connected");
+
+  if (!players.white) {
+    players.white = uniquesoket.id;
+    uniquesoket.emit("playerRole", "w");
+  } else if (!players.black) {
+    players.black = uniquesoket.id;
+    uniquesoket.emit("playerRole", "b");
+  } else {
+    uniquesoket.emit("spectatorRole");
+  }
+
+  uniquesoket.on("disconnect", function () {
+    if (uniquesoket.id === players.white) {
+      delete players.white;
+    } else if (uniquesoket.id === players.black) {
+      delete players.black;
+    }
+  });
 });
 
 server.listen(3000, function () {
